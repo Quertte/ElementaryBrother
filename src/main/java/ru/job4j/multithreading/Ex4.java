96195acce42858d4fb4771d6e555189f93fd7326
Quertte <a.injen90@gmail.com>
2022-02-17 10:29:55 +0300

    Варианты создания потоков

---
+++ src/main/java/ru/job4j/multithreading/Ex4.java
@@ -0,0 +1,13 @@
+package ru.job4j.multithreading;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Privet");
+            }
+        }).start();
+        new Thread(() -> System.out.println("Privet")).start();
+    }
+}
