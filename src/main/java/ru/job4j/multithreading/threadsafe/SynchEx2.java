ff41ba02b009fbc4a61ee3560d58f2a5c10356ae
Quertte <a.injen90@gmail.com>
2022-02-19 17:16:00 +0300

    Synchronized Collection

---
+++ src/main/java/ru/job4j/multithreading/threadsafe/SynchEx2.java
@@ -0,0 +1,35 @@
+package ru.job4j.multithreading.threadsafe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class SynchEx2 {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            arrayList.add(i);
+        }
+        List<Integer> synchList = Collections.synchronizedList(arrayList);
+
+        Runnable runnable = () -> {
+            synchronized (synchList) {
+                Iterator<Integer> iterator = synchList.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+                }
+            }
+        };
+        Runnable runnable1 = () -> {
+            synchList.remove(10);
+        };
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable1);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(synchList);
+    }
+}
