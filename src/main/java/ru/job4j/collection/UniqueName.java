0f7d52edc92cb00e90a08cfc54a68d80431197fd
Quertte <a.injen90@gmail.com>
2022-06-09 16:23:46 +0300

    0.1. Уникальные имена

---
+++ src/main/java/ru/job4j/collection/UniqueName.java
@@ -0,0 +1,48 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
+public class UniqueName {
+
+    public static int collectUniqueName(List<User> users) {
+        return new HashSet<>(users).size();
+    }
+
+    public static class User {
+        private final int id;
+        private final String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
+}
